9b6039d9ef5ee5035badf7b609d2bed17b50793e
ivog <ivo@igsoft.nl>
2018-05-03 02:02:48 +0200

    fix4

--- OrderPizza.cs
+++ OrderPizza.cs
@@ -15,7 +15,7 @@ namespace Deloitte.CqrsAzureDemo
         [FunctionName("OrderPizza")]
         public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, 
-            [EventHub("deloittedemoeventhub")] ICollector<string> outputEvents,
+            [EventHub("deloittedemoeventhub", Connection = "EventHubConnectionString")] ICollector<string> outputEvents,
             TraceWriter log)
         {
 
